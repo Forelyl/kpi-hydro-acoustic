00ba93fcf2d4f69e7546c8405808a767843eab65
Forelyl <wolf1in1forest@gmail.com>
2025-01-05 19:14:52 +0200

    add debug and release types of run start of adding aditional plot info for debug
    Co-authored-by: Naz <Naz2204@users.noreply.github.com>

--- Backend/server/main.py
+++ Backend/server/main.py
@@ -2,7 +2,31 @@ from fastapi import FastAPI
 from routers import debug, function_call, help
 from fastapi.middleware.cors import CORSMiddleware
 
-app = FastAPI()
+from contextlib import asynccontextmanager
+import sys
+from datetime import datetime
+from functions.functions import Audio_track
+
+
+# runtime context manager
+@asynccontextmanager
+async def app_lifecycle_context(app: FastAPI):
+    is_debug: bool = "--reload" in sys.argv
+    Audio_track.set_use_function(is_debug)
+
+    # Start info
+    print("=" * 30)
+    print("Starting application...")
+    print("Time:", datetime.now())
+    print("Debug:", is_debug)
+    print("=" * 30)
+
+    yield
+
+    return
+
+# FastAPI main object
+app = FastAPI(lifespan=app_lifecycle_context)
 
 # routers
 app.include_router(debug.app)
